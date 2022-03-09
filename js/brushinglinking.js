cc43852bc452fb3c0e443f8f4d2b9214abdfe5d9
Liu Conghan <liuconghan@MacBook-Pro-2.local>
2022-03-08 22:10:35 -0500

    three chart link

--- js/brushinglinking.js
+++ js/brushinglinking.js
@@ -190,78 +190,65 @@ d3.csv("data/iris.csv").then((data) => {
     
 
     svg2.call(brush2);
-                    
-
-
 
   }
 
   //TODO: Barchart with counts of different species
   {
     // Bar chart code here 
-    let xKey3 = "Species";
-    let yKey3 = "Count"
+    xKey3 = "Species";
+    yKey3 = "Count";
 
-
-    let x3 = d3.scaleLinear()
-                .domain([0, 3])
-                .range([margin.left, width-margin.right]);
-
-    let y3 = d3.scaleLinear()
-                .domain([0, 50])
-                .range([height - margin.bottom, margin.top]);
-
-    let svg3 =  d3.select("#vis-holder")
-                  .append("svg")
-                  .attr("width", width-margin.left-margin.right)
-                  .attr("height", height-margin.top-margin.bottom)
-                  .attr("viewBox", [0, 0, width, height]);
-    
   
-//     svg3.append("g")
-//         .attr("transform", `translate(${margin.left}, 0)`) 
-//         .call(d3.axisLeft(y3)) 
-//         .attr("font-size", '20px'); 
+    const data1 = [{name:"setosa", count:"50"},
+                   {name:"versicolor", count:"50"},
+                  {name:"virginica", count:"50"}];
 
-// // Add x axis to webpage  
-//     svg3.append("g")
-//         .attr("transform", `translate(0,${height - margin.bottom})`) 
-//         .call(d3.axisBottom(x3))  
-//         .attr("font-size", '20px');
+  let maxY3 = 50; 
+
+   x3 = d3.scaleBand()
+                .domain(d3.range(data1.length))
+                .range([margin.left, width-margin.right])
+                .padding(0.1);
+
+   y3 = d3.scaleLinear()
+              .domain([0, maxY3])
+                .range([height - margin.bottom, margin.top]);
 
     svg3.append("g")
         .attr("transform", `translate(0,${height - margin.bottom})`) 
         .call(d3.axisBottom(x3))   
-        .attr("font-size", '20px')
+        .attr("font-size", '25px')
+        .call(d3.axisBottom(x3) 
+                .tickFormat(i => data1[i].name)) 
         .call((g) => g.append("text")
                       .attr("x", width - margin.right)
                       .attr("y", margin.bottom - 4)
                       .attr("fill", "black")
                       .attr("text-anchor", "end")
-                      .text(xKey3)
-        );
+                      .text(xKey3));
 
     svg3.append("g")
         .attr("transform", `translate(${margin.left}, 0)`) 
         .call(d3.axisLeft(y3)) 
-        .attr("font-size", '20px') 
+        .attr("font-size", '20px')
         .call((g) => g.append("text")
                       .attr("x", 0)
                       .attr("y", margin.top)
                       .attr("fill", "black")
                       .attr("text-anchor", "end")
-                      .text(yKey3)
-      );
+                      .text(yKey3));
 
     svg3.selectAll(".bar") 
-        .data(data) 
+        .data(data1) 
         .enter()  
         .append("rect") 
         .attr("class", "bar") 
-        .attr("x", (d) => x3(d.Species)) 
-        .attr("y", (d,i) => y3(i)) 
-        .attr("height", (d) => (height - margin.bottom) - y3(d)) 
+        .attr("x", (d,i) => x3(i)) 
+        .attr("y", (d) => y3(d.count)) 
+        .attr("height", (d) => (height - margin.bottom) - y3(d.count)) 
         .attr("width", x3.bandwidth())
+        .style("fill",(d)=>color(d.name));
   }
 
   //Brushing Code---------------------------------------------------------------------------------------------
@@ -277,33 +264,38 @@ d3.csv("data/iris.csv").then((data) => {
 
   // Call when Scatterplot1 is brushed 
   function updateChart1(brushEvent) {
-    let extent = brushEvent.selection
-    myCircles1.classed("selected", function(d){ return isBrushed(extent, x1(d.Sepal_Length), y1(d.Petal_Length) ) } )
+    
     
       
       //TODO: Find coordinates of brushed region 
-  
+      let extent = brushEvent.selection
       //TODO: Give bold outline to all points within the brush region in Scatterplot1
-
-      //TODO: Give bold outline to all points in Scatterplot2 corresponding to points within the brush region in Scatterplot1
+      myCircles1.classed("selected", function(d){ return isBrushed(extent, x1(d.Sepal_Length), y1(d.Petal_Length) ) } )
     
+      //TODO: Give bold outline to all points in Scatterplot2 corresponding to points within the brush region in Scatterplot1
+      myCircles2.classed("selected", function(d){return isBrushed(extent, x1(d.Sepal_Length), y1(d.Petal_Length))})
   }
 
   // Call when Scatterplot2 is brushed 
   function updateChart2(brushEvent) {
-    let extent = brushEvent.selection
-    myCircles2.classed("selected", function(d){ return isBrushed(extent, x2(d.Sepal_Width), y2(d.Petal_Width) ) } )
+    
+    
     
     //TODO: Find coordinates of brushed region 
-
+    let extent = brushEvent.selection
     //TODO: Start an empty set that you can store names of selected species in 
-  
+    let selectSpecies = new Set()
     //TODO: Give bold outline to all points within the brush region in Scatterplot2 & collected names of brushed species
-
+    myCircles2.classed("selected", function(d){
+      if (isBrushed(extent, x2(d.Sepal_Width), y2(d.Petal_Width)))
+      {
+        selectSpecies.add(d.name)
+      }
+       return isBrushed(extent, x2(d.Sepal_Width), y2(d.Petal_Width))  } )
     //TODO: Give bold outline to all points in Scatterplot1 corresponding to points within the brush region in Scatterplot2
-
+    myCircles1.classed("selected", function(d){ return isBrushed(extent, x2(d.Sepal_Width), y2(d.Petal_Width) ) } )
     //TODO: Give bold outline to all bars in bar chart with corresponding to species selected by Scatterplot2 brush
-
+    svg3.classed("selected", function(d){return selectSpecies.isBrushed(d.name)});
   }
 
     //Finds dots within the brushed region
